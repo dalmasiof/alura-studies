33c53c469649f0de76b0089ea850dd2e15c20b8e
Dalmasio Fernandes <dalmasiof@gmail.com>
2022-09-04 00:04:33 -0300

    creating components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from './components/Botao';
 
 function App() {
   return (
     <div className="App">
-       <h1>HELLO WORLD IN REACT TS!!</h1>
+       <h1>HELLO WORLD IN REACT ON  TS!!</h1>
+       <div>
+        <Button/>
+       </div>
     </div>
   );
 }
---
+++ src/components/Botao.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Button extends React.Component{
+     render(): React.ReactNode {
+         return (
+            <div>
+                <button>
+                    Click here!!!
+                </button>
+            </div>
+         )
+     }
+}
